26252a4893a0856bf45d63fa25f438e33c57732a
Dazz Kaminski <kaminski.darek@icloud.com>
2020-05-26 08:02:50 +0100

    Code cleanup

--- src/main/java/com/booklibrary/frontend/config/CoreConfiguration.java
+++ src/main/java/com/booklibrary/frontend/config/CoreConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CoreConfiguration implements WebMvcConfigurer {
 
+  // TODO Front-end security
+
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
